f5dcdbccaabbc01754f540112f6e309dbbf9071d
Paulina-Dyulgerska <paylina_st@yahoo.com>
2022-01-17 17:30:08 -0800

    Session 1 solution

---
+++ Session1/Session1/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text;
+
+Console.WriteLine("The entered text would be displayed in the console window in reversed order and if there are two identical characters next to each other, they will be replaced with a single character with the same value.");
+
+var input = Console.ReadLine();
+
+var stringBuilder = new StringBuilder();
+
+if (!string.IsNullOrWhiteSpace(input))
+{
+    for (int i = input.Length - 1; i >= 0; i--)
+    {
+        if (i > 0 && input[i - 1] == input[i])
+        {
+            continue;
+        }
+        stringBuilder.Append(input[i]);
+    }
+}
+
+Console.WriteLine(stringBuilder.ToString());
