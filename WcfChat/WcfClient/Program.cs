9e05a0127daaeea1b147b16b2b97faf453696e42
Paulina-Dyulgerska <paylina_st@yahoo.com>
2022-01-24 12:20:19 -0800

    Add dynamic service IP

--- WcfChat/WcfClient/Program.cs
+++ WcfChat/WcfClient/Program.cs
@@ -8,30 +8,47 @@ namespace WcfClient
     {
         static void Main(string[] args)
         {
-            var chatClient = new ChatClient(new InstanceContext(new ChatClientCallBack()));
-            Console.WriteLine("Enter your username: ");
-            var username = Console.ReadLine();
-            if (string.IsNullOrWhiteSpace(username))
+            //var chatClient = new ChatClient(new InstanceContext(new ChatClientCallBack()));
+            Console.WriteLine("Enter service IP: ");
+            var ip = Console.ReadLine();
+            if (string.IsNullOrWhiteSpace(ip))
             {
-                Console.WriteLine("Username is required. Bye!");
+                Console.WriteLine("Service IP is required. Bye!");
                 return;
             }
 
-            var user = new User { Name = username };
-            var userConnected = chatClient.Connect(user);
-            if (userConnected == false)
+            try
             {
-                Console.WriteLine($"Cannot join Elders' chat room. Please contact room admin.");
-                return;
-            }
-            Console.WriteLine($"Hello {user.Name} and welcome to Elders' chat room.");
+                var chatClient = new ChatClient(new InstanceContext(new ChatClientCallBack()), "NetTcpBinding_IChat", new EndpointAddress($"net.tcp://{ip}:50820/Chat/tcp"));
 
-            string message;
-            do
+                Console.WriteLine("Enter your username: ");
+                var username = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(username))
+                {
+                    Console.WriteLine("Username is required. Bye!");
+                    return;
+                }
+
+                var user = new User { Name = username };
+                var userConnected = chatClient.Connect(user);
+                if (userConnected == false)
+                {
+                    Console.WriteLine($"Cannot join Elders' chat room. Please contact room admin.");
+                    return;
+                }
+                Console.WriteLine($"Hello {user.Name} and welcome to Elders' chat room.");
+
+                string message;
+                do
+                {
+                    message = Console.ReadLine();
+                    chatClient.Say(new ChatMessage() { Content = message, Sender = user.Name, Time = DateTime.Now });
+                } while (message != "exit");
+            }
+            catch (Exception ex)
             {
-                message = Console.ReadLine();
-                chatClient.Say(new ChatMessage() { Content = message, Sender = user.Name, Time = DateTime.Now });
-            } while (message != "exit");
+                Console.WriteLine($"An exception occurred: {ex.Message}");
+            }
         }
     }
 }
