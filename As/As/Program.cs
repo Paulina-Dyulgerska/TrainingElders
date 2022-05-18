9012c079e9a17597d38d874e369a841bd4a70442
Paulina-Dyulgerska <paylina_st@yahoo.com>
2022-05-18 10:50:28 +0300

    As playground

---
+++ As/As/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using As;
+
+Console.WriteLine("Hello, World!");
+
+var gg0 = new Test { Asd = 12 };
+var gg1 = new Test { Asd = 13 };
+var gg2 = new Test { Asd = 14 };
+var gList = new List<Parent> { gg0, gg1, gg2 };
+
+foreach (var item in gList)
+{
+    var t = item as Test; // if ggo, gg1 or gg2 are not created as Test, but as something else, t will be null
+    var a = t.Asd;
+    var b = t.MyProperty;
+}
+
+Console.ReadLine();
---
+++ As/As/Test.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace As
+{
+    public class Test : Parent
+    {
+        public Test()
+        {
+            if (MyProperty.HasValue == false)
+            {
+                MyProperty = 4;
+            }
+        }
+
+        public int? MyProperty { get; set; }
+    }
+
+    public class Parent
+    {
+        public int Asd { get; set; }
+    }
+}
